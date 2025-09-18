1e71fa2dcadfec5dbb2297a703220a40faef0097
arjtar <arjantpaudel@gmail.com>
2025-09-18 15:19:05 +0545

    practise

--- dsa1.py
+++ dsa1.py
@@ -75,7 +75,7 @@ print(smallerNumbersThanCurrent(nums))
 
 #---------------------------------------------
 
-#Single number
+# 5 Single number
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
         xor = 0
@@ -88,7 +88,7 @@ nums = [4, 1, 2, 1, 2,4,6]
 print(s.singleNumber(nums))
 
 #---------------------------------------------
-#coinchange
+# 6 coinchange
 
 def coinchange(coins, amount):
 
@@ -104,7 +104,40 @@ print(coinchange([1, 2, 5], 11))
 
 
 
+#---------------------------------------------
+# 7 climbing stairs
 
+def steps(n):
+    if n ==0:
+        return 0
+    if n ==1:
+        return 1
+    if n == 2:
+        return 2
+    
+    dp = [0] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    for i in range (3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+print(steps(4))
 
+#---------------------------------------------
+#8 Maxsum subarray
 
- 
+def maxSubArray(arr):
+    maxSum = arr[0]
+    currentSum = 0
+
+    for n in arr:
+        if currentSum < 0:
+            currentSum = 0
+
+        currentSum += n
+        maxSum = max(maxSum, currentSum)
+    return maxSum
+
+arr = [-2, 1, -3, -1, 2, 1, -5]
+print(maxSubArray(arr))
