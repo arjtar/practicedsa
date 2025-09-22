d598695fd63c0fc23af155cdf9d25ade4a2b2726
arjtar <arjantpaudel@gmail.com>
2025-09-22 07:57:22 +0545

    added problem solution

--- dsa1.py
+++ dsa1.py
@@ -197,3 +197,49 @@ def combine(n,k):
     backtrack(1, [])
     return result 
 print(combine(4,2))
+
+
+#---------------------------------------------
+#12.Middle of the linked list
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def hasCycle(self, head):
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+            
+        return False
+    
+node1 = ListNode(3)
+node2 = ListNode(2)
+node3 = ListNode(0)
+node4 = ListNode(5)
+node5 = ListNode(7)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2 
+
+s = Solution()
+print(s.hasCycle(node1))  
+
+
+
+
+
+
+    
+
+
+
+        
